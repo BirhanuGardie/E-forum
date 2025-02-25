318faf4cbaad969b2f2aaf7f8e332bfd9de36ac3
BirhanuGardie <106751124+BirhanuGardie@users.noreply.github.com>
2025-02-25 14:45:11 +0300

    "updated"

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,9 @@ function App() {
       }
 
       const userData = await axiosInstance
-        .get("/users/check", { headers: { Authorization: "Bearer " + token } })
+        .get("/users/check", { headers: { 
+          Authorization: "Bearer " + token }
+         })
         .then((response) => response.data);
       console.log(userData);
       setUser(userData); // Store the user data in state so that it can be accessed by others too
@@ -41,79 +43,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// const express = require('express');
-
-//          // db connection
-// const dbconnection = require('./db/dbConfig')
-
-
-
-
-// const app = express();
-// const port = 5500
-
-
-// // sample to test the server is working
-// // app.get(`/`, (req, res)=> {
-// // res.send("welcome")
-// // })
-
-// // user routes middleware file
-// const useRoutes = require("./routes/userRoute")
-
-// // user routes middleware 
-// app.use("/app/users", useRoutes)
-
-// // questions routes middleware ??
-
-
-// // answers routes middleware ??
-
-// async function start() {
-//     try {
-//     //   const result = await dbconnection.execute("select 'test' ") 
-//     const [result] = await dbconnection.query("SELECT 'test' AS result");
-
-//         console.log(result)
-//        await app.listen(port)
-//         console.log("database connection established")
-//         // console.log(`listing on ${port}`)
-//      } catch (error) {
-//          console.log(error.message);
-         
-//      }
-     
-// }
-//  start() 
-
-
-
-
-
-// const mysql2 = require('mysql2');
-
-// const dbconnection = mysql2.createPool({
-//     user:"Evangadi-admin", 
-//     database:"evangadi-forum",
-//     host:"localhost",
-//     password:"123456",
-//     connectionLimit: 10
-// })
-
-
-// // when we insert data for  register,answer,question....will happen call backheal so we should turn to PROMISE based
-// // dbconnection.execute( "select 'test' ", (err,result)=> {
-
-// //     if(err) {
-// //         console.log(err.message);
-// //     }else{
-// //         console.log(result);
-
-// //     }
-// // })
-
-
-// module.exports = dbconnection.promise; 
